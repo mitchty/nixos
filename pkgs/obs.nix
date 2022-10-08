e720dc196a55b630ae0d37d4213632879f5bd4ec
Mitchell Tishmack <git@mitchty.net>
2022-10-08 09:35:45 -0500

    wip

---
+++ pkgs/obs.nix
@@ -0,0 +1,26 @@
+{ stdenv
+, lib
+, pkgs
+}:
+with pkgs; stdenv.mkDerivation rec {
+  name = "obs-studio";
+  uname = "obsproject";
+  aname = "OBS";
+  version = "28.0.3";
+
+  sourceRoot = ".";
+  phases = [ "installPhase" ];
+  installPhase = ''
+    install -dm755 "$out/Applications"
+    VOLUME=`/usr/bin/hdiutil attach $src | grep Volumes | awk '{print $3}'`
+    cp -rf $VOLUME/${aname}.app $out/Applications/${aname}.app
+    /usr/bin/hdiutil detach $VOLUME
+  '';
+
+  src = fetchurl {
+    name = "${aname}.dmg";
+    # As of obs 28.0.0 this disk image is now APFS and not HFS, so undmg no worky
+    url = "https://github.com/${uname}/${name}/releases/download/${version}/obs-studio-${version}-macos-x86_64.dmg";
+    sha256 = "sha256-QOpFGphkbeq+KpQwbG+S4O/jmctt6hU8drwzcPrBNQQ=";
+  };
+}
