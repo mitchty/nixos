bfe95b99cc5ca8f3871b459c0e70011a04363ac8
Mitchell Tishmack <git@mitchty.net>
2022-10-15 11:25:02 -0500

    wireshark: 4.0.0

--- pkgs/wireshark.nix
+++ pkgs/wireshark.nix
@@ -5,7 +5,7 @@
 with pkgs; stdenv.mkDerivation rec {
   name = "wireshark";
   gname = "Wireshark";
-  version = "3.6.8";
+  version = "4.0.0";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -18,7 +18,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${gname}.dmg";
     url = "https://2.na.dl.wireshark.org/osx/Wireshark%20${version}%20Intel%2064.dmg";
-    sha256 = "sha256-weVPGvkzSGrGDalLsaNm31EllZ70FuGpPCovud5476A=";
+    sha256 = "sha256-OIDNmtNczbIzp1dGHdyIIfJr4KR3H8vEbM1tetyhWQM=";
   };
   latest = "curl --location --silent https://www.wireshark.org/#download | htmlq -wpt | awk '/current stable/ {print $8}' | sed 's/.$//'";
 
