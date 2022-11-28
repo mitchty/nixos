ab913e94a68c574bb2b1bb1fa64152c239381cdc
Mitchell Tishmack <git@mitchty.net>
2022-11-27 18:47:46 -0600

    xq: 1.1.0

--- pkgs/xq.nix
+++ pkgs/xq.nix
@@ -2,23 +2,21 @@
 , lib
 , pkgs
 }:
-# Like minio...ish
-# TODO: Get this building again on macos, some weird cgo things going on with go-ieproxy
-# Leaving not working efforts inline for future me to know what doesn't work.
+
 with pkgs; buildGo118Module rec {
   # https://github.com/sibprogrammer/xq/releases
   oname = "sibprogrammer";
   pname = "xq";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = oname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-EJ7byYl3VoflbNH0JFbr1zWWNVXpioN3yKwCEzSmVGc=";
+    sha256 = "sha256-P6ZGxle/+UfDDRpsohAndYIA2RqlCsFF79bJM4yu4m0=";
   };
 
-  vendorSha256 = "sha256-9XvXoXwebs6TRlfxXidiRkQLzgZYkDo8o/a4A0SO26s=";
+  vendorSha256 = "sha256-Rv/MvyvTQecK+ZgdAcec6lpO6KYzZY0eGfDX7iI7AP4=";
 
   meta.mainProgram = "xq";
 
