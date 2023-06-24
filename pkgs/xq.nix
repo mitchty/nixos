4b888a6c1499556c8d663ec76e00a991ed82f9e4
Mitchell Tishmack <git@mitchty.net>
2023-06-23 20:09:03 -0500

    xq: 1.1.4

--- pkgs/xq.nix
+++ pkgs/xq.nix
@@ -7,16 +7,16 @@ with pkgs; buildGo118Module rec {
   # https://github.com/sibprogrammer/xq/releases
   oname = "sibprogrammer";
   pname = "xq";
-  version = "1.1.3";
+  version = "1.1.4";
 
   src = fetchFromGitHub {
     owner = oname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-6to8ZSD3rjFUDy/xGhUjpiIk2vDjXJcTPXSG6/1BYNo=";
+    sha256 = "sha256-bwyQ16E1M/vxwZQG472r0ZN7pVwZpb5wEot/Jt/Z6Ak=";
   };
 
-  vendorSha256 = "sha256-HI+6A027yvHb/ZyXdt/pRSdmYbNYXGrkmZ4EFb4MqXc=";
+  vendorSha256 = "sha256-5WLPLH8jsAQfx/OMJ9IZPPvqW0rXvnJf6PsFnVJH+CU=";
 
   meta.mainProgram = "xq";
 
