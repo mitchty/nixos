27a7b0469c44584231e5df4089aeb123e7cda094
Mitchell Tishmack <git@mitchty.net>
2023-06-23 20:09:51 -0500

    obs-studio: 29.1.3

--- pkgs/obs-studio.nix
+++ pkgs/obs-studio.nix
@@ -6,7 +6,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "obs-studio";
   uname = "obsproject";
   aname = "OBS";
-  version = "29.1.2";
+  version = "29.1.3";
 
   sourceRoot = ".";
   phases = [ "installPhase" ];
@@ -21,7 +21,7 @@ with pkgs; stdenv.mkDerivation rec {
     name = "${aname}.dmg";
     # As of obs 28.0.0 this disk image is now APFS and not HFS, so undmg no worky
     url = "https://github.com/${uname}/${name}/releases/download/${version}/obs-studio-${version}-macos-x86_64.dmg";
-    sha256 = "sha256-pERf8PrW6BT4pha13YBxhjkswYuU5dKawng28E7nYcc=";
+    sha256 = "sha256-DocFHNXuUPnvuckFLXmj1Zh2GxVDCCE8QKzKzDydCJU=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/obsproject/obs-studio/releases/latest' | jq -r '.tag_name'";
