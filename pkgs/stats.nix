5390c6b79dbd42ebf7e9f1f6baaa8f890586f75a
Mitchell Tishmack <git@mitchty.net>
2023-07-15 14:40:30 -0500

    stats: 2.9.2

--- pkgs/stats.nix
+++ pkgs/stats.nix
@@ -7,7 +7,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "stats";
   uname = "exelban";
   aname = "Stats";
-  version = "2.8.26";
+  version = "2.9.2";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -19,7 +19,7 @@ with pkgs; stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/${uname}/${name}/releases/download/v${version}/${aname}.dmg";
-    sha256 = "sha256-GktEugJSBoOwpsGSOI9ZPDbd5NFceEoi3M8Mrv6B6Lc=";
+    sha256 = "sha256-1rXC4b0N9uga0fX5vZQEdxoH77ruNXtWoaDCNWVgtFA=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/exelban/stats/releases/latest' | jq -r '.tag_name' | tr -d v";
