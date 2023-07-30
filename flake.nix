dd6821cedd8478f513c0c092aa7ee788959ac137
Mitchell Tishmack <git@mitchty.net>
2023-07-29 21:25:49 -0500

    chore: just some silly cleanup

--- flake.nix
+++ flake.nix
@@ -3,22 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    yolo.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust.url = "github:oxalica/rust-overlay";
   };
 
   # TODO: Future me, nuke flake-utils
-  outputs = { nixpkgs, unstable, flake-utils, rust, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, yolo, flake-utils, rust, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
+      pkgs-nixpkgs = import nixpkgs {
         inherit system;
         overlays = [
           (import rust)
         ];
       };
-      stable = nixpkgs.legacyPackages.${system};
-      yolo = unstable.legacyPackages.${system};
+
+      pkgs-yolo = import yolo {
+        inherit system;
+        overlays = [
+          (import rust)
+        ];
+      };
+
+      pkgs = pkgs-nixpkgs;
 
       fake = nixpkgs.legacyPackages.${system}.lib.fakeSha256;
 
@@ -33,27 +40,27 @@
       # TODO: Make all this subpackages n stuff, will do it piecemeal with what
       # updates most often first.
       packages = {
-        altshfmt = pkgs.callPackage ./pkgs/altshfmt.nix { pkgs = stable; makeWrapper = pkgs.makeWrapper; };
-        gh-actions-status = pkgs.callPackage ./pkgs/gh-actions-status.nix { pkgs = stable; };
-        helm-unittest = pkgs.callPackage ./pkgs/helm-unittest.nix { pkgs = stable; };
-        jira-cli = pkgs.callPackage ./pkgs/jira-cli.nix { pkgs = stable; };
-        no-more-secrets = pkgs.callPackage ./pkgs/no-more-secrets.nix { pkgs = stable; };
+        altshfmt = pkgs.callPackage ./pkgs/altshfmt.nix { inherit pkgs; makeWrapper = pkgs.makeWrapper; };
+        gh-actions-status = pkgs.callPackage ./pkgs/gh-actions-status.nix { inherit pkgs; };
+        helm-unittest = pkgs.callPackage ./pkgs/helm-unittest.nix { inherit pkgs; };
+        jira-cli = pkgs.callPackage ./pkgs/jira-cli.nix { inherit pkgs; };
+        no-more-secrets = pkgs.callPackage ./pkgs/no-more-secrets.nix { inherit pkgs; };
         transcrypt = pkgs.callPackage ./pkgs/transcrypt.nix { fetchFromGitHub = pkgs.fetchFromGitHub; git = pkgs.git; openssl = pkgs.openssl; coreutils = pkgs.coreutils; util-linux = pkgs.util-linux; gnugrep = pkgs.gnugrep; gnused = pkgs.gnused; gawk = pkgs.gawk; }; # Upstream nixpkgs is ancient vendor it in and pr if its ok.
       } // (pkgs.lib.optionalAttrs (system == "x86_64-darwin") {
-        nheko = pkgs.callPackage ./pkgs/nheko.nix { pkgs = stable; };
-        obs-studio = pkgs.callPackage ./pkgs/obs-studio.nix { pkgs = stable; };
-        stats = pkgs.callPackage ./pkgs/stats.nix { pkgs = stable; };
-        stretchly = pkgs.callPackage ./pkgs/stretchly.nix { pkgs = stable; };
-        swiftbar = pkgs.callPackage ./pkgs/swiftbar.nix { pkgs = stable; };
-        vlc = pkgs.callPackage ./pkgs/vlc.nix { pkgs = stable; };
-        wireshark = pkgs.callPackage ./pkgs/wireshark.nix { pkgs = stable; };
+        nheko = pkgs.callPackage ./pkgs/nheko.nix { inherit pkgs; };
+        obs-studio = pkgs.callPackage ./pkgs/obs-studio.nix { inherit pkgs; };
+        stats = pkgs.callPackage ./pkgs/stats.nix { inherit pkgs; };
+        stretchly = pkgs.callPackage ./pkgs/stretchly.nix { inherit pkgs; };
+        swiftbar = pkgs.callPackage ./pkgs/swiftbar.nix { inherit pkgs; };
+        vlc = pkgs.callPackage ./pkgs/vlc.nix { inherit pkgs; };
+        wireshark = pkgs.callPackage ./pkgs/wireshark.nix { inherit pkgs; };
       }) // (pkgs.lib.optionalAttrs (system == "x86_64-linux") {
-        hponcfg = pkgs.callPackage ./pkgs/hponcfg.nix { fetchurl = pkgs.fetchurl; rpmextract = stable.rpmextract; openssl = pkgs.openssl; busybox = pkgs.busybox; autoPatchelfHook = pkgs.autoPatchelfHook; makeWrapper = pkgs.makeWrapper; };
+        hponcfg = pkgs.callPackage ./pkgs/hponcfg.nix { fetchurl = pkgs.fetchurl; rpmextract = pkgs.rpmextract; openssl = pkgs.openssl; busybox = pkgs.busybox; autoPatchelfHook = pkgs.autoPatchelfHook; makeWrapper = pkgs.makeWrapper; };
       }) // flake-utils.lib.flattenTree {
-        hwatch = pkgs.callPackage ./pkgs/hwatch.nix { pkgs = stable; };
+        hwatch = pkgs.callPackage ./pkgs/hwatch.nix { inherit pkgs; };
 
-        hatools = pkgs.callPackage ./pkgs/hatools.nix { pkgs = stable; };
-        xq = pkgs.callPackage ./pkgs/xq.nix { pkgs = stable; };
+        hatools = pkgs.callPackage ./pkgs/hatools.nix { inherit pkgs; };
+        xq = pkgs.callPackage ./pkgs/xq.nix { inherit pkgs; };
 
         default = pkgs.stdenv.mkDerivation {
           name = "mitchty";
