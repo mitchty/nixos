fc0e8075e2ebd0d94f717459d7b71c57bea03baa
Mitchell Tishmack <git@mitchty.net>
2023-08-25 10:12:01 -0500

    stats: 2.9.6

--- pkgs/stats.nix
+++ pkgs/stats.nix
@@ -7,19 +7,22 @@ with pkgs; stdenv.mkDerivation rec {
   name = "stats";
   uname = "exelban";
   aname = "Stats";
-  version = "2.9.4";
+  version = "2.9.6";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
   phases = [ "unpackPhase" "installPhase" ];
   installPhase = ''
     install -dm755 "$out/Applications"
-    cp -r ${aname}.app "$out/Applications/${aname}.app"
+    VOLUME=$(/usr/bin/hdiutil attach -nobrowse $src | awk '/Volumes/ {print $3}')
+    trap "/usr/bin/hdiutil detach $VOLUME" EXIT
+    APP="$(echo $VOLUME/*.app)"
+    cp -rf "$APP" "$out/Applications/$(basename "$APP")"
   '';
 
   src = fetchurl {
     url = "https://github.com/${uname}/${name}/releases/download/v${version}/${aname}.dmg";
-    sha256 = "sha256-TfVm65Wa0fNZo1Pwd210vHI4t/xyfBdj1/LXqxtxRgY=";
+    sha256 = "sha256-rbv8YlBfq0cEqv8Hwoi5Qmai2ROl3IHdmSd9SCCZtSM=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/exelban/stats/releases/latest' | jq -r '.tag_name' | tr -d v";
