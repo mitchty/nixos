3bceb4151816d9c9d0507af70fc815e3c2c501c9
Mitchell Tishmack <git@mitchty.net>
2023-10-07 08:38:10 -0500

    keepingyouawake: 1.6.5

--- pkgs/keepingyouawake.nix
+++ pkgs/keepingyouawake.nix
@@ -6,7 +6,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "newmarcel";
   uname = "KeepingYouAwake";
   aname = "KeepingYouAwake";
-  version = "1.6.4";
+  version = "1.6.5";
 
   buildInputs = [ unzip ];
   sourceRoot = ".";
@@ -20,7 +20,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${aname}.zip";
     url = "https://github.com/${name}/${uname}/releases/download/${version}/${uname}-${version}.zip";
-    sha256 = "sha256-jKPJLHjxUEGgVs24Xs2eKY0Q8ZnL0p1Xsr+xyFjnbfs=";
+    sha256 = "sha256-Tp2HnJXL+OJTcJdMK4h18Xsqo7nClbt20Np0zYM6/gQ=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/${name}/${uname}/releases/latest' | jq -r '.tag_name'";
