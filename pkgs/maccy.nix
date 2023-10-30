a194ce879f30137d19c9ac6381bfedee53a78252
Mitchell Tishmack <git@mitchty.net>
2023-10-30 12:47:45 -0500

    maccy: 0.28.0

--- pkgs/maccy.nix
+++ pkgs/maccy.nix
@@ -6,7 +6,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "p0deje";
   uname = "maccy";
   aname = "Maccy";
-  version = "0.27.1";
+  version = "0.28.0";
 
   buildInputs = [ unzip ];
   sourceRoot = ".";
@@ -20,7 +20,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${aname}.app.zip";
     url = "https://github.com/${name}/${aname}/releases/download/${version}/${aname}.app.zip";
-    sha256 = "sha256-q09b6HBQNEI/Q4Nl4CrawgSLXtPIPQH32ghK3Vhsa5o=";
+    sha256 = "sha256-dxjt5skIHN6VlkWpcmj+ZSovVARuQETKoyKMkMtUhHQ=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/${name}/${aname}/releases/latest' | jq -r '.tag_name'";
