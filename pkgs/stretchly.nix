5bdc7e661fdc925eed3e78572a7c47078dc0e15e
Mitchell Tishmack <git@mitchty.net>
2023-11-22 09:56:22 -0600

    stretchly: 1.15.1

--- pkgs/stretchly.nix
+++ pkgs/stretchly.nix
@@ -6,7 +6,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "stretchly";
   uname = "hovancik";
   aname = "Stretchly";
-  version = "1.14.1";
+  version = "1.15.1";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -19,7 +19,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     # https://github.com/hovancik/stretchly/releases/download/v1.10.0/Stretchly-1.10.0.dmg
     url = "https://github.com/${uname}/${name}/releases/download/v${version}/${aname}-${version}.dmg";
-    sha256 = "sha256-CSu4wxXLjLpMBLeGfJTYJteJ/kABQ8YWz+wdGhkDHxI=";
+    sha256 = "sha256-zbnpwYYmJTmqGs8ElJGho8zRLpcyJF/vlNf54oIZghg=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/hovancik/stretchly/releases/latest' | jq -r '.tag_name' | tr -d v";
