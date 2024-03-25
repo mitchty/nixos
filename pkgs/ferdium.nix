66e720b9bf24a9b53bcc7030c5143b6bffac1bca
Mitchell Tishmack <git@mitchty.net>
2024-03-25 15:42:58 -0500

    ferdium: 0.3.11

--- pkgs/ferdium.nix
+++ pkgs/ferdium.nix
@@ -6,7 +6,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "ferdium";
   uname = "ferdium";
   aname = "Ferdium";
-  version = "6.7.0";
+  version = "6.7.1";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -19,7 +19,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${aname}.dmg";
     url = "https://github.com/${uname}/${name}-app/releases/download/v${version}/${aname}-mac-${version}-x64.dmg";
-    sha256 = "sha256-yMe3uUJmJgYGwTqANwRQ791ZQ0Y4rIz92re2S/21Stc=";
+    sha256 = "sha256-Uj2fvr3DfdVpRkC7teavXuVlkZEVKHHXABsJmKk1tUo=";
   };
 
   latest = "curl --silent 'https://api.github.com/repos/${uname}/${name}-app/releases/latest' | jq -r '.tag_name' | tr -d v";
