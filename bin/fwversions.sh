d5515dcfc7a993a1d9e5a9d284e7c845aa9af649
Mitchell Tishmack <git@mitchty.net>
2024-03-31 12:20:20 -0500

    Make fwversions a bit more chatty when/if puppeteer is angy

--- bin/fwversions.sh
+++ bin/fwversions.sh
@@ -10,7 +10,7 @@ export _base _dir
 
 set "${SETOPTS:--eu}"
 
-ok=0
+ok=1
 
 TEMP=${TMPDIR:-/tmp}
 T="${TEMP}/${_base}-$$"
@@ -18,26 +18,26 @@ T="${TEMP}/${_base}-$$"
 install -dm755 "${T}"
 
 cleanup() {
-  [[ -d "${T}" ]] && rm -fr "${T}"
+  [ -d "${T}" ] && rm -fr "${T}"
+  [ ${ok} -ne 0 ] && printf "note: %s did not succesfully try again with SETOPTS=-eux\n" "${_base}" >&2
+  exit $ok
 }
 
 trap cleanup EXIT
 
 cd "${T}"
 
-popts="--no-sandbox https://tascam.com/us/product/mixcast_4/download test.pdf"
-
-puppeteer print ${popts} > /dev/null 2>&1
+puppeteer print --no-sandbox https://tascam.com/us/product/mixcast_4/download test.pdf > /dev/null 2>&1
 
 curr="https://tascam.com/downloads/products/tascam/mixcast_4/mixcast4_fw_v131.zip"
 latest=$(pdfx -v test.pdf | grep -E '(mixcast_4.*_fw_.*.zip)' | sort -ur | head -n1 | awk '{print $2}')
 
 if [[ "${curr}" != "${latest}" ]]; then
-  ok=$((ok+1))
+  ok=$((ok + 1))
   old=$(echo ${curr} | awk -F\_v '{print $2}' | tr -d '.zip')
   new=$(echo ${latest} | awk -F\_v '{print $2}' | tr -d '.zip')
   printf "mixcast 4 firmware skew current=%s found=%s\n" "${old}" "${new}"
   printf "change curr to: %s\n" "${latest}"
+else
+  ok=0
 fi
-
-exit $ok
