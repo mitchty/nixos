16d08b0e0de42e44219e77ab0519e4ce83a70522
Mitchell Tishmack <git@mitchty.net>
2024-03-31 12:11:31 -0500

    ferdium: 4.2.4

--- pkgs/wireshark.nix
+++ pkgs/wireshark.nix
@@ -5,7 +5,7 @@
 with pkgs; stdenv.mkDerivation rec {
   name = "wireshark";
   gname = "Wireshark";
-  version = "4.2.3";
+  version = "4.2.4";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -18,7 +18,7 @@ with pkgs; stdenv.mkDerivation rec {
   src = fetchurl {
     name = "${gname}.dmg";
     url = "https://2.na.dl.wireshark.org/osx/Wireshark%20${version}%20Intel%2064.dmg";
-    sha256 = "sha256-zqAtPTbBy4Voq+tCpQtRaaJv0Xmjcm9EUeFnxhJDuEY=";
+    sha256 = "sha256-APbwhlImPUqTJJ/xuo/f8o3haAYYTZ0m4Nuhq4ue1gQ=";
   };
   latest = "curl -s https://gitlab.com/api/v4/projects/7898047/repository/tags?order_by=version | jq -r '.[].name' | awk '!/rc/ {print $1;exit}' | sed -e 's/wireshark-//g'";
 
