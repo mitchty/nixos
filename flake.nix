ce3e127b09788f5fc935303bc5cc2b3c52295379
Mitchell Tishmack <git@mitchty.net>
2024-07-21 10:03:50 -0500

    Switch nixpkgs to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   description = "My out of band flakes/pkgs/modules for NixOS/Darwin";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/90f456026d284c22b3e3497be980b2e47d0b28ac";
+    #nixpkgs.url = "github:NixOS/nixpkgs/90f456026d284c22b3e3497be980b2e47d0b28ac";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     rust = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
