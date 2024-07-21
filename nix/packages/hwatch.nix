4e472b82269ff681f52967d1000d8899af91b344
Mitchell Tishmack <git@mitchty.net>
2024-07-21 10:03:22 -0500

    hwatch 0.3.15

--- nix/packages/hwatch.nix
+++ nix/packages/hwatch.nix
@@ -6,17 +6,17 @@
 }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "hwatch";
-  version = "0.3.13";
+  version = "0.3.15";
 
   src = pkgs.fetchFromGitHub {
     owner = "blacknon";
     repo = pname;
     rev = version;
-    sha256 = "sha256-3RFiVDXjPFBMK+f/9s9t3cdIH+R/88Fp5uKbo5p2X+g=";
+    sha256 = "sha256-UmNxdp9acRCKnUsKw7Z9z3knRvVkqQ5atxR/dqpGBYE=";
     forceFetchGit = true;
   };
 
-  cargoSha256 = "sha256-MC0Ch9ai4XmhhRz/9nFYEA3A7kgBv2x9q4yc5IJ7CZ8=";
+  cargoSha256 = "sha256-pEhogmK2WBj/PxcDtJs/H0XZhPiz3zCQMX2eUcAfnTE=";
 
   # passthru.tests.version = pkgs.testVersion {
   #   package = hwatch; };
